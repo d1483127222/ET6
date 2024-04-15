112b7c110403e13416d92214ce1117fa28a9b2be
zhangdongliang <1483127222@qq.com>
2024-04-15 08:42:20 +0800

    登录2

---
+++ Server/Model/Demo/Account/Account.cs
@@ -0,0 +1,20 @@
+﻿namespace ET
+{
+
+    public enum AccountType
+    {
+        General = 0,//正常账号
+        BlackList = 1,//黑名单
+    }
+
+    public class Account : Entity,IAwake
+    {
+        public string AccountName;//账户名
+
+        public string Password; //账户密码
+
+        public long CreateTime;//账号创建时间
+
+        public int AccountType;//账号类型
+    }
+}
